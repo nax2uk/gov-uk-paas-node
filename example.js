38e173616759ad4f82a79e37a551a7f1b5b72863
nax2uk <azlinayeo@gmail.com>
2021-03-25 14:46:29 +0000

    edited example.js to make it work

--- example.js
+++ example.js
@@ -1,45 +1,23 @@
 const express = require('express')
 const app = express()
-// const port = process.env.PORT || 3000;
 const cfenv = require("cfenv");
 const pg = require('pg');
 const appEnv = cfenv.getAppEnv();
 const connectionString = appEnv.getServiceURL("my-pg-service");
-const client = new pg.Client(connectionString);
-client.ssl = true;
-client.connect();
+const client = new pg.Client({ connectionString, ssl: true });
 
 client
-.query('SELECT * FROM public.test_table')
-.then(result=>console.log('*****result' + result))
-.catch(error=>console.error(error.stack))
-.then(()=>client.end())
+.connect()
+.then(()=>console.log('connected'))
+.catch(err => console.error('connection error', err.stack))
 
 app.get('/', (req, res) => {
-  console.log("In the first GET call");
-  res.send('Hello World!')
+  client
+  .query('SELECT * FROM public.id')
+  .then(result=>res.send(`These are the id values ${JSON.stringify(result.rows)}`))
+  .catch(error=>console.error(error.stack))
+  
 })
-
-
-console.log("******** example.js ************");
-console.log(appEnv);
-console.log("connection string: ", connectionString);
-
-// app.get("/", async (req, res) => {
-//   console.log("************* In the GET request **************");
-//   try {
-//     console.log("In the try block");
-//     const response = await client.query("SELECT * FROM public.test_table");
-//     console.log(response);
-//     res.send('Hello World!');
-//   } catch (err) {
-//     console.log("In the catch block");
-//     console.log(err);
-//     res.send(err);
-//   }
-// });
-
-
 
 app.listen(appEnv.port, appEnv.bind, () => {
   console.log(`Example app listening at http://localhost:${appEnv.port}`)
